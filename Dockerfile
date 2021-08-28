35cff9419721db2c113946e9feb8d17b2d9f29f6
fletch <bassmattf@gmail.com>
2021-08-28 12:48:21 -0500

    basic working app created

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.7-alpine
 COPY . /app
 WORKDIR /app
 RUN pip3 install -r requirements.txt --user
+EXPOSE 5000
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/dashboard/<name>')
+def dashboard(name):
+   return 'welcome %s' % name
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
 
+@app.route('/',methods = ['POST', 'GET'])
+def home():
+    return "HOME PAGE"
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('dashboard',name = user))
+   else:
+      user = request.args.get('name')
+      return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug = True, host='0.0.0.0', port=5000)
